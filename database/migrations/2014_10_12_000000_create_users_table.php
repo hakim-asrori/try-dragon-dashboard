7217582990c8372a4c9669c34ed75527557e3a61
hakim-asrori <saneglos005@gmail.com>
2026-04-29 20:22:05 +0700

    update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,16 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('f_name',100)->nullable();
-            $table->string('l_name',100)->nullable();
+            $table->string('f_name', 100)->nullable();
+            $table->string('l_name', 100)->nullable();
             $table->string('phone')->nullable();
-            $table->string('email',100)->nullable();
-            $table->string('image',100)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('image', 100)->nullable();
             $table->boolean('is_phone_verified')->default(0);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password',100);
+            $table->string('password', 100);
             $table->string('email_verification_token')->nullable();
-            $table->string('cm_firebase_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
